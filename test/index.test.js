d5a3b0883e0db8473f21546cc5d6794b5cab2c52
Nicholas Reno <nick@livioradio.com>
2016-08-24 10:23:55 -0400

    second commit

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,6 @@ var test = require('../examples/richErrorTest.js');
 
 describe('Rich-Error', function(){
 	it('should work!', function(){
-		expect(true).to.be.true;
+		expect(false).to.be.true;
 	});
 });
