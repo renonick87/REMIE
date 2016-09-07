775624a50676ac980b40cc3e04c0ea736abdcb1c
Nicholas Reno <nick@livioradio.com>
2016-09-07 17:37:45 -0400

    tried to fix the weird way remies and richerrors are made

--- examples/better-example.js
+++ examples/better-example.js
@@ -2,15 +2,15 @@
  * ******************** REMIE
  * ************************************************** */
 let REMIE = require('../libs/index.js'),
-  remie = new (require('../libs/index.js'))(),
+//  remie = new (require('../libs/index.js'))(),
   i18next = require('i18next'),
   EventEmitter = require('events');
   //EventListener = require('events').EventListener;
   //console.log(EventListener)
 var inherits = require('util').inherits;
 err = 'server.400.notFound'
-err.code = 'server.400.notFound'
-err.message = 'Message in an error!'
+//err.code = 'server.400.notFound'
+//err.message = 'Message in an error!'
 //err.stack = 'errors stacked on errors'
 //err.internalMessage = 'internal message'
 
@@ -20,13 +20,21 @@ options.level = 'warning'
 //options.code = 'server.400.notFound'
 
 let locale = 'server.400.notFound'
-
-remie.i18next = i18next
+let remie = new REMIE(err, options, locale) //rich Error
+//let remie = REMIE.methodCaller()
+//remie.i18next = i18next
 
 inherits(REMIE, EventEmitter.EventEmitter)
-
+//let error = remie.methodCaller() // remie
+let other = remie.create(err, options, locale) 
+let copy = remie.copy(other)
+console.log(other)
+console.log(copy)
+//if (error == other) {
+//  console.log('wooh')
+//}
 //let error = remie.create(err, options, locale)
-let error = remie.create(err, options)
+//let error = remie.create(err, options)
 
 //let error = remie.create('Something went wrong', options, locale)
 //let error = remie.create("Something went wrong", {})
@@ -35,7 +43,7 @@ let error = remie.create(err, options)
 //let error2 = remie.toObject(error)
 //let error2 = remie.create("Something else went wrong", {});
 //let error3 = remie.create("Oh crap, what now", {});
-console.log(error)
+//console.log(error)
 
 //module.exports = error
 
